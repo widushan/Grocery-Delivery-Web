dc065803c63dac2ec11497f997d3aab4a346dbd7
widushan <114930601+widushan@users.noreply.github.com>
2025-11-15 23:49:19 +0530

    nav commit
    nav bar done

---
+++ cartigo/client/src/context/AppContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+
+  const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin};
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useAppContext = () => {
+  return useContext(AppContext)
+}
