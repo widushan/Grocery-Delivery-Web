dad0b996bb6e99c8eeb16d98ca3950f69d007046
widushan <114930601+widushan@users.noreply.github.com>
2025-11-16 20:36:31 +0530

    category commit
    category started

---
+++ cartigo/client/src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Categories = () => {
+  return (
+    <div>
+      <p>Categories</p>
+      <div>
+        <div>
+          <img src={assets.box_icon} alt="" />
+          <p>fruit</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Categories
