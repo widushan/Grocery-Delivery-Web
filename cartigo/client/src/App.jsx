2361c53315b78beb25a5f9f02e458e1dddc65c82
widushan <114930601+widushan@users.noreply.github.com>
2025-12-03 03:06:36 +0530

    bestSeller commit
    designing the Best Seller part

--- cartigo/client/src/App.jsx
+++ cartigo/client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
 
@@ -10,6 +11,8 @@ const App = () => {
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+
+      <Toaster />
       
       <div className={`${isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
         <Routes>
---
+++ cartigo/client/src/components/BestSeller.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+import { useAppContext } from '../context/AppContext'
+
+const BestSeller = () => {
+
+    const {products} = useAppContext();
+    
+  return (
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
+
+      <div>
+        < ProductCard product={products[0]} />
+      </div>
+      
+    </div>
+  )
+}
+
+
+export default BestSeller
