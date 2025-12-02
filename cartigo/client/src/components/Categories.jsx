44bf2ab74a9e05106f867d67db6effb5738eab8c
widushan <114930601+widushan@users.noreply.github.com>
2025-12-03 02:01:58 +0530

    category commit
    main category part done

--- cartigo/client/src/components/Categories.jsx
+++ cartigo/client/src/components/Categories.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { categories } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 const Categories = () => {
+
+    const {navigate} = useAppContext()
+
   return (
-    <div>
-      <p>Categories</p>
-      <div>
-        <div>
-          <img src={assets.box_icon} alt="" />
-          <p>fruit</p>
-        </div>
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Categories</p>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6'>
+
+        {categories.map((category, index)=>{
+          return (
+            <div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center' 
+              style={{backgroundColor: category.bgColor}} 
+              onClick={()=>{
+                navigate(`/products/${category.path.toLowerCase()}`);
+                scrollTo(0,0)
+              }}>
+              <img src={category.image} alt={category.text} className='group-hover:scale-108 transition max-w-28' />
+              <p className='text-sm font-medium'>{category.text}</p>
+            </div>
+          )
+        })}
+
+
       </div>
     </div>
   )
 }
+
 
 export default Categories
